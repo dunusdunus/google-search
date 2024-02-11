3d993318cb5738cbf0a4731381642e25588012d6
dunusdunus <d.sudarkin803@gmail.com>
2024-02-11 20:43:41 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const buttonLuck = document.getElementById('buttonLuck');
+const buttonSearch = document.getElementById('buttonSearch');
+
+buttonLuck.addEventListener('click', function() {
+    alert('Тебе точно повезёт :)');
+});
+
+buttonSearch.addEventListener('click', function() {
+    alert('Поиск пока не работает :(');
+});
